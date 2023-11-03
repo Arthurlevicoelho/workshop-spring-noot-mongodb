5cdc0194664987a7314164edab0f618e191e4ac5
Arthur Coelho <arthurlevicoelho@hotmail.com>
2023-11-03 17:47:59 -0300

    Project init

---
+++ src/main/java/com/arthurlevi/worshopmongod/WorshopmongodApplication.java
@@ -0,0 +1,13 @@
+package com.arthurlevi.worshopmongod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshopmongodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshopmongodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurlevi/worshopmongod/WorshopmongodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurlevi.worshopmongod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopmongodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
